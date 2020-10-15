fdfd6a3c9d230d41af7f5a72dc592d8a0c83b5eb
AlanBarros1999 <61669052+AlanBarros1999@users.noreply.github.com>
2020-10-14 21:21:25 -0300

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questao02;
+
+/**
+ *
+ * @author Alan
+ */
+/**
+ *  A classe Bike recebe as milhas anuais e as calorias gastas por milha
+ *  por uma bicicleta e calcula sua pegada de carbono
+ *
+ */
+
+//São gastos 34 calorias a cada milha, utilizando bicicletas.
+//Fórmula = (carbon footprint = milhas anuais * 34).
+
+public class Bike implements CarbonFootprint {
+
+	private double milhasAno;
+	private final int caloriasPorMilha = 34; //calorias gastas por milha
+
+	/**
+	 * Constructor que recebe a quantidade de milhas gastas no ano
+	 * 
+	 * @param milhas - milhas gastas no ano
+	 * 
+	 */
+	public Bike( double milhas ) {
+		milhasAno = milhas;
+	}
+
+	/**
+	 * Metodo que obtem a quantidade de milhas gastas no ano
+	 * 
+	 * @param milhas - milhas gastas no ano
+	 * 
+	 */
+	public void setMilhasAno( double milhas ) {
+		milhasAno = milhas;
+	}
+
+	/**
+	 * Metodo que retorna a quantidade de milhas gastas no ano
+	 * 
+	 * @return milhas gastas no ano
+	 * 
+	 */
+	public double getMilhasAno() {
+		return milhasAno;
+	}
+
+	/**
+	 * Metodo que imprime a quantidade de milhas gastas no ano
+	 * 
+	 * @return milhas gastas no ano
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return String.format("%s: %.2f", "Milhas anuais ", getMilhasAno());
+	}
+
+	/**
+	 * Metodo que calcula a pegada de carbono produzida por bicicletas
+	 * 
+	 * @return CarbonFootprint de bikes
+	 * 
+	 */
+	@Override
+	public double getCarbonFootprint() {
+		return milhasAno * caloriasPorMilha;
+	}
+}   
+
---
+++ CarbonFootprint.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questao02;
+
+/**
+ *
+ * @author Alan
+ */
+/**
+ * Classe interface do programa
+ * 
+ */
+
+public interface CarbonFootprint {
+	double getCarbonFootprint(); //Recebe o carbon footprint(pegada de carbono)
+}
+
